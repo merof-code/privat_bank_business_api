015ecc487f2937519f3b919f9010deec8c711f59
merof <misha.rosh@gmail.com>
2025-02-24 22:26:45 +0000

    init

---
+++ lib/privat_bank_buisness_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "privat_bank_buisness_api/version"
+
+module PrivatBankBuisnessApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
